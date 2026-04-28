421736a68d28eaead92ff54e7374a1bbfc3ff9d3
Metehan Yildirim <metegenez@gmail.com>
2026-04-28 17:27:26 +0100

    fix(docker): set JAVA_HOME in be.conf so JDBC connector doesn't crash BE on query close

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -14,6 +14,9 @@ echo "Container IP: $CONTAINER_IP (network: $CONTAINER_SUBNET)"
 echo "priority_networks = $CONTAINER_SUBNET" >> /etc/starrocks/fe/fe.conf
 echo "priority_networks = $CONTAINER_SUBNET" >> /etc/starrocks/be/be.conf
 
+# Set JAVA_HOME for BE (start_be.sh --daemon drops Dockerfile ENV; JDBC connector needs it)
+echo "JAVA_HOME = /usr/lib/jvm/java-17-openjdk-amd64" >> /etc/starrocks/be/be.conf
+
 echo "=== Starting StarRocks FE ==="
 /usr/lib/starrocks/fe/bin/start_fe.sh --daemon
 
